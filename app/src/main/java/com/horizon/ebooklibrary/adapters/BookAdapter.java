2cad09726cae181affeff9d16befdcc9a304364c
Horizon Halifa <OrHalifa1@gmail.com>
2025-03-03 17:42:44 +0200

    Implemented Recycler View

--- app/src/main/java/com/horizon/ebooklibrary/adapters/BookAdapter.java
+++ app/src/main/java/com/horizon/ebooklibrary/adapters/BookAdapter.java
@@ -15,13 +15,31 @@ import com.horizon.ebooklibrary.model.Book;
 
 import java.util.List;
 
-public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder>{
+/* This class extends RecyclerView.Adapter, meaning it controls how items appear in the RecyclerView.
+ * It uses a custom ViewHolder to store the views for each book item.
+ */
+public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder> {
+
+    /*
+     * How it works?
+     * RecyclerView asks the Adapter how many items there are ( getItemCount ).
+     * RecyclerView calls onCreateViewHolder to create views for the items.
+     * RecyclerView calls onBindViewHolder to bind data to those views.
+     * The user scrolls -> The RecyclerView reuses views to improve performance.
+     */
+
 
     private Context context;
     private List<Book> bookList;
 
+    /*
+     * The costume ViewHolder class:
+     * Hold references to UI elements (textTitle and textAuthor).
+     * findViewById links these to book_item.xml.
+     */
     public static class BookViewHolder extends RecyclerView.ViewHolder {
-        private TextView textTitle, textAuthor;
+        TextView textTitle, textAuthor;
+
         public BookViewHolder(@NonNull View itemView) {
             super(itemView);
             textTitle = itemView.findViewById(R.id.textTitle);
@@ -36,12 +54,22 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder
 
     @NonNull
     @Override
+    /*
+     * inflate(book_item.xml) -> Creates a view for a single book item.
+     * Wrap it in a BookViewHolder -> So it can hold references to it's TextViews.
+     */
     public BookAdapter.BookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.book_item, parent, false);
         return new BookViewHolder(view);
     }
 
     @Override
+    /*
+     * This method is called when a new book needs to be displayed.
+     * Steps:
+     * 1. Get the book at position.
+     * 2. Set it's title & author in TextViews.
+     */
     public void onBindViewHolder(@NonNull BookAdapter.BookViewHolder holder, int position) {
         Book book = bookList.get(position);
         holder.textTitle.setText(book.getTitle());
@@ -49,6 +77,7 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder
     }
 
     @Override
+    // Returns the total number of books, so the RecyclerView knows how  many to display.
     public int getItemCount() {
         return bookList.size();
     }
